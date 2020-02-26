987ed87b61397de92e9c37538104430b8900fb46
Manmeet Kalra <manmeetskalra@gmail.com>
2020-02-25 16:11:54 -0800

    Add files via upload

---
+++ ass2_part3.R
@@ -0,0 +1,33 @@
+library(lubridate)
+library(chron)
+library(dplyr)
+library(magrittr)
+
+
+#Extract the data for group 05
+Datadf <- read.table("Data_Assignmet2.txt", header = TRUE, sep = ",")
+newdate=parse_date_time(Datadf$Date, orders = c("ymd", "dmy", "mdy"))
+Datadf$week=strftime(newdate, format = "%V", tz = "", usetz = FALSE)
+data <- Datadf[Datadf$week == '05',]
+
+n <- factor(data$Time)
+a_time <- hms(as.character(n))
+data$hour <- as.integer(hour(a_time))
+newdata <- data[c("hour","Global_active_power")]
+time_array = array(data = NA, dim = nrow(newdata)-6)
+GAP_array = array(data = NA, dim = nrow(newdata)-6)
+
+i <- 1
+j <- 7
+while (j < nrow(newdata)) {
+  test_window <- newdata %>% slice(i:j)
+  #time_array[i] <- mean(test_window$hour, na.rm = TRUE)
+  GAP_array[i] <- mean(test_window$Global_active_power, na.rm = TRUE)
+  i = i+1
+  j = j+1
+}
+
+plot(GAP_array, type="l",
+     main = "Global Active Power Average per Window",
+     xlab = "Window", ylab = "Global Active Power Average")
+
