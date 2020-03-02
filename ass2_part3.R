b95e23b9c9471fadc426c5193bca20dda4694879
Manmeet Kalra <manmeetskalra@gmail.com>
2020-03-02 15:58:28 -0800

    Add files via upload

--- ass2_part3.R
+++ ass2_part3.R
@@ -2,32 +2,56 @@ library(lubridate)
 library(chron)
 library(dplyr)
 library(magrittr)
+library(ggplot2)
 
 
-#Extract the data for group 05
+
+data <- read.table("Data_Assignmet2.txt", header = TRUE, sep = ",")
 Datadf <- read.table("Data_Assignmet2.txt", header = TRUE, sep = ",")
-#newdate=parse_date_time(Datadf$Date, orders = c("ymd", "dmy", "mdy"))
-#Datadf$week=strftime(newdate, format = "%V", tz = "", usetz = FALSE)
-#data <- Datadf[Datadf$week == '05',]
 data <- Datadf
 n <- factor(data$Time)
 a_time <- hms(as.character(n))
 data$hour <- as.integer(hour(a_time))
-newdata <- data[c("hour","Global_active_power")]
-time_array = array(data = NA, dim = nrow(newdata)-20)
-GAP_array = array(data = NA, dim = nrow(newdata)-20)
+
+newdata <- data[c("Date","Global_active_power")]
+newdata$Date <- as.Date(newdata$Date, format='%d/%m/%Y')
+
+averaged_days <- data[c("hour","Global_active_power")]
+#aggregate(Global_active_power ~ Date, newdata, mean)
+GAP_array = array(data = NA, dim = nrow(averaged_days)-20)
 
 i <- 1
 j <- 20
-while (j < nrow(newdata)) {
-  test_window <- newdata %>% slice(i:j)
-  #time_array[i] <- mean(test_window$hour, na.rm = TRUE)
+while (j < nrow(averaged_days)) {
+  test_window <- averaged_days %>% slice(i:j)
   GAP_array[i] <- mean(test_window$Global_active_power, na.rm = TRUE)
   i = i+1
   j = j+1
 }
 
-plot(GAP_array, type="p",
-     main = "Global Active Power Average per Window",
-     xlab = "Window", ylab = "Global Active Power Average")
+df <- data.frame(GAP_array)
+df$row_num <- seq.int(nrow(df))
 
+plot(GAP_array, type="l",
+     main = "Global Active Power Average per Window",
+     xlab = "Days", ylab = "Global Active Power Average")
+
+
+
+averaged_days <- aggregate(Global_active_power ~ Date, newdata, mean)
+window_frame = array(data = NA, dim = nrow(averaged_days)-6)
+i <- 1
+j <- 6
+
+while(j < nrow(averaged_days)) {
+  test_window <- averaged_days %>% slice(i:j)
+  window_frame[i] <- mean(test_window$Global_active_power, na.rm = TRUE)
+  i = i+1
+  j = j+1
+}
+
+dd <- data.frame(window_frame)
+dd$row_num <- seq.int(nrow(dd))
+plot(window_frame, type="l",
+     main = "Global Active Power Average per Window",
+     xlab = "Days", ylab = "Global Active Power Average")
